61f5e03de6186deb94e665bc7af28e7d1341b0f8
Anirudh Joshi <github@anirudhjoshi.com>
2011-07-21 07:52:24 +0000

    Updated readme.md for automated installs. Created install/uninstall/update scripts to make vim-pathogen more enjoyable to use. Fully automated installation.

---
+++ readme.md
@@ -0,0 +1,27 @@
+This project adds [pathogen] to the vim editor. It manages your runtime path.
+
+[pathogen]: http://www.vim.org/scripts/script.php?script_id=2332
+
+### Installing
+
+```bash
+curl https://raw.github.com/anirudhjoshi/vim-pathogen/master/install.sh | sh
+```
+
+That's it.
+
+### Updating
+
+```bash
+curl https://raw.github.com/anirudhjoshi/vim-pathogen/master/update.sh | sh
+```
+
+Everything will then be brought up to date.
+
+### Uninstalling
+
+```bash
+curl https://raw.github.com/anirudhjoshi/vim-pathogen/master/uninstall.sh | sh
+```
+
+vim-pathogen will be uninstalled.
---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+return_dir=`pwd`
+
+echo && echo "Downloading updated pathogen." && echo
+
+cd ~/.vim/autoload/
+wget -qN https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+if grep -qEe '(filetype plugin off|call pathogen#infect\(\)|filetype indent on)' ~/.vimrc
+then
+	echo "Pathogen already in .vimrc. Passing..." && echo
+else
+
+	sed -i '1i filetype plugin off\ncall pathogen#infect()\nfiletype indent on' ~/.vimrc
+	
+	echo "Prepended pathogen init to .vimrc." && echo
+fi
+
+cd $return_dir
+
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo && echo "vim-pathogen failed to update." >&2
+else
+	 echo && echo "vim-pathogen was updated."
+fi
+
+exit $ret
